bc5c85710ada7954ea66a3eae994ee0ad0027a90
TalysonSoares <talysonsoares40@gmail.com>
2022-11-02 20:45:53 -0300

    Trabalhando na lógica do programa

--- events.js
+++ events.js
@@ -4,10 +4,31 @@ let lista = document.querySelector('select#flista');
 let res =  document.querySelector('div#res');
 let valores = []
 
-function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value)) {
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
 
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     } else {
         alert('Valor inválido ou já encontrado na lista.')
     }
+    num.value = ''
+    num.focus();
 }
