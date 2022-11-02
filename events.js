eb666c13dfa229eedbdfb79751adbdb5f7148d33
TalysonSoares <talysonsoares40@gmail.com>
2022-11-02 20:12:26 -0300

    trabalhando na estrutura da condição

--- events.js
+++ events.js
@@ -1,8 +1,13 @@
 // let num = document.getElementById('fnum')
-let num = document.querySelector('input#fnum')
-
-
+let num = document.querySelector('input#fnum');
+let lista = document.querySelector('select#flista');
+let res =  document.querySelector('div#res');
+let valores = []
 
 function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value)) {
 
+    } else {
+        alert('Valor inválido ou já encontrado na lista.')
+    }
 }
