c556b38d57f42f12159245031c7ac5b842eca914
TalysonSoares <talysonsoares40@gmail.com>
2022-11-02 21:28:26 -0300

    Finalizando projeto

--- events.js
+++ events.js
@@ -2,7 +2,7 @@
 let num = document.querySelector('input#fnum');
 let lista = document.querySelector('select#flista');
 let res =  document.querySelector('div#res');
-let valores = []
+let valores = [];
 
 function isNumero(n) {
     if(Number(n) >= 1 && Number(n) <= 100) {
